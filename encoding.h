0f9286d5964f61e434eef12d70574886f41ad05e
Peter Verthez <verthezp@users.sourceforge.net>
2001-11-24 22:27:12 +0000

    Better interface for selection of encoding.

--- encoding.h
+++ encoding.h
@@ -1,5 +1,17 @@
+/* Basic file encoding */
+#ifndef __ENCODING_H
+#define __ENCODING_H
+
+typedef enum _ENC {
+  ONE_BYTE = 0,
+  TWO_BYTE_HILO = 1,
+  TWO_BYTE_LOHI = 2
+} ENCODING;
+
 int open_conv_to_internal(char* fromcode);
 void close_conv_to_internal();
 char* to_internal(char* str, size_t len);
 void init_encodings();
-char* get_encoding(char* gedcom_name);
+void set_encoding_width(ENCODING enc);
+
+#endif /* __ENCODING_H */
