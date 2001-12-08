9269f512611266c80c2000751d2badfd9b87537e
Peter Verthez <verthezp@users.sourceforge.net>
2001-12-08 15:08:19 +0000

    Moved some parts to the external header.

--- gedcom.h
+++ gedcom.h
@@ -28,24 +28,12 @@
 #define MAXGEDCPTRLEN   22
 #define GEDCOMTAGOFFSET 257
 
-/* Error handling mechanisms */
-typedef enum _MECH {
-  IMMED_FAIL,
-  DEFER_FAIL,
-  IGNORE_ERRORS
-} MECHANISM;
-
-
 int        gedcom_error(char* s, ...);
 int        gedcom_warning(char* s, ...);
 int        gedcom_message(char* s, ...);
 int        gedcom_debug_print(char* s, ...);
-void       gedcom_set_debug_level(int level);
-void       gedcom_set_error_handling(MECHANISM mechanism);
-void       gedcom_set_compat_handling(int enable_compat);
 
 int        gedcom_parse();
-
 int        gedcom_lex();
 
 extern int line_no;
