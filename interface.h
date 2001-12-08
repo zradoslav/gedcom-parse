445649c6d8a2799fc27f32bf31ab698672b0d915
Peter Verthez <verthezp@users.sourceforge.net>
2001-12-08 21:28:14 +0000

    Some changes to the interface...

--- interface.h
+++ interface.h
@@ -4,13 +4,14 @@
 #include "gedcom.h"
 #include "external.h"
 
-Gedcom_ctxt start_record(Gedcom_rec rec, char *xreftag);
+Gedcom_ctxt start_record(Gedcom_rec rec, int level, char *xref, char *tag);
 void        end_record(Gedcom_rec rec, Gedcom_ctxt self);
 
 Gedcom_ctxt start_element(Gedcom_elt elt, Gedcom_ctxt parent,
 			  int level, char *tag, char *raw_value,
-			  void *parsed_value);
-void        end_element(Gedcom_ctxt parent, Gedcom_ctxt self);
+			  Gedcom_val parsed_value);
+void        end_element(Gedcom_elt elt, Gedcom_ctxt parent, Gedcom_ctxt self,
+			Gedcom_val parsed_value);
 
 
 #endif /* __INTERFACE_H */
