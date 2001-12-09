a0ec77497ccbad0476f54fdd722bb37a613a9480
Peter Verthez <verthezp@users.sourceforge.net>
2001-12-09 17:26:45 +0000

    Changes due to name change.

--- include/gedcom.h
+++ include/gedcom.h
@@ -1,5 +1,17 @@
-#ifndef __EXTERNAL_H
-#define __EXTERNAL_H
+/*  This program is free software; you can redistribute it and/or modify  *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation; either version 2 of the License, or     *
+ *  (at your option) any later version.                                   *
+
+ (C) 2001 by The Genes Development Team
+ Original author: Peter Verthez (Peter.Verthez@advalvas.be)
+*/
+
+/* $Id$ */
+/* $Name$ */
+
+#ifndef __GEDCOM_H
+#define __GEDCOM_H
 
 #include <stdio.h>
 
@@ -97,4 +109,4 @@ void    gedcom_subscribe_to_element(Gedcom_elt elt,
 				    Gedcom_elt_start_cb cb_start,
 				    Gedcom_elt_end_cb cb_end);
 
-#endif /* __EXTERNAL_H */
+#endif /* __GEDCOM_H */
