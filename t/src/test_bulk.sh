0f30245c7100cc031e3167ad992055e240fff61b
Peter Verthez <verthezp@users.sourceforge.net>
2002-11-03 15:48:19 +0000

    Refactored common parts into test_prologue.sh and test_bulk.sh.

---
+++ t/src/test_bulk.sh
@@ -0,0 +1,41 @@
+# $Id$
+# $Name$
+# Common test part for the test scripts
+
+ltcmd="$builddir/../libtool --mode=execute"
+for lib in $test_libs
+do
+  ltcmd="$ltcmd -dlopen $lib"
+done
+
+echo "======================================================">> $logfile
+echo "Performing test '$test_name'" >> $logfile
+$ltcmd $GEDCOM_TESTENV $builddir/src/$test_program $options $test_args
+result=$?
+rm gedcom.enc
+rm new.ged
+rm ANSI_Z39.47.so
+rm gconv-modules
+
+echo "Result is $result (expected: $expected_result)" >> $logfile
+if [ "$result" -ne "$expected_result" ]
+then
+  echo "Not the expected return value!" >> $logfile
+  exit 1
+else
+  if [ -r core ]
+  then
+    echo "Core file generated!" >> $logfile
+    exit 1
+  else
+    if diff $outfile $reffile >/dev/null 2>>$logfile
+    then
+      echo "Output agrees with reference output" >> $logfile
+      rm $outfile
+      exit 0
+    else
+      echo "Differences with reference output detected!" >> $logfile
+      exit 1
+    fi
+  fi
+fi
---
+++ t/src/test_prologue.sh
@@ -0,0 +1,50 @@
+# $Id$
+# $Name$
+# Common prologue for the test scripts
+
+extra_options=
+
+while [ $# -gt 0 ]
+do
+  case "$1" in
+    -*) extra_options="$extra_options $1";;
+    *)  break;;
+  esac
+  shift
+done
+
+test_name=$1
+shift
+expected_result=$1
+shift
+if [ -z "$test_name" ]
+then
+  test_name=check
+fi
+if [ -z "$expected_result" ]
+then
+  expected_result=0
+fi
+
+# For use outside Makefile
+if [ -z "$srcdir" ]
+then
+  :
+else
+  options="-q $extra_options"
+fi
+
+outfile=$test_name.out
+logfile=check.out
+reffile=$srcdir/output/$test_name.ref
+options="$options -o $outfile"
+
+GCONV_PATH=.:$GCONV_PATH
+export GCONV_PATH
+LANG=C
+export LANG
+ln -s $srcdir/../data/gedcom.enc .
+ln -s $builddir/../data/new.ged .
+ln -s $builddir/../iconv/glibc/.libs/ANSI_Z39.47.so .
+ln -s $srcdir/../iconv/glibc/gconv-modules .
+rm -f core
