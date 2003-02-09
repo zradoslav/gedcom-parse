a233f109e951c78311ea01fac5efa7a528939806
Peter Verthez <verthezp@users.sourceforge.net>
2003-02-09 09:55:49 +0000

    Split off context stuff to different file.

--- gom/gom.c
+++ gom/gom.c
@@ -52,19 +52,6 @@
 #include "gom.h"
 #include "gom_internal.h"
 
-const char* ctxt_names[] =
-{
-  "NULL",
-  
-  "header", "submission", "submitter", "family", "individual",
-  "multimedia", "note", "repository", "source", "user_rec",
-  
-  "address", "event", "place", "source_citation", "text",
-  "note_sub", "multimedia_link", "lds_event", "user_ref_number",
-  "change_date", "personal_name", "family_link", "pedigree",
-  "association", "source_event", "source_description"
-};
-
 void gom_default_callback (Gedcom_elt elt, Gedcom_ctxt parent, int level,
 			   char* tag, char* raw_value, int parsed_tag);
 
@@ -179,99 +166,12 @@ int gom_write_xref_list(Gedcom_write_hndl hndl,
   return result;
 }
 
-Gom_ctxt make_gom_ctxt(int ctxt_type, OBJ_TYPE obj_type, void *ctxt_ptr)
-{
-  Gom_ctxt ctxt   = (Gom_ctxt)malloc(sizeof(struct Gom_ctxt_struct));
-  if (! ctxt)
-    MEMORY_ERROR;
-  else {
-    ctxt->ctxt_type = ctxt_type;
-    ctxt->obj_type  = obj_type;
-    ctxt->ctxt_ptr  = ctxt_ptr;
-  }
-  return ctxt;
-}
-
-void NULL_DESTROY(void* anything UNUSED)
-{
-}
-
-void destroy_gom_ctxt(Gom_ctxt ctxt)
-{
-  SAFE_FREE(ctxt);
-}
-
-void gom_cast_error(const char* file, int line,
-		    OBJ_TYPE expected, OBJ_TYPE found)
-{
-  const char* expected_name = "<out-of-bounds>";
-  const char* found_name    = "<out-of-bounds>";
-  if (expected < T_LAST)
-    expected_name = ctxt_names[expected];
-  if (found < T_LAST)
-    found_name = ctxt_names[found];
-  fprintf(stderr,
-	  "Wrong gom ctxt cast at %s, line %d: expected %s, found %s\n",
-	  file, line, expected_name, found_name);
-  abort();
-}
-
-void gom_mem_error(const char *filename, int line)
-{
-  gedcom_error(_("Could not allocate memory at %s, %d"), filename, line);
-}
-
-void gom_xref_already_in_use(const char *xrefstr)
-{
-  gedcom_error(_("Cross-reference key '%s' is already in use"), xrefstr);
-}
-
-void gom_unexpected_context(const char* file, int line, OBJ_TYPE found)
-{
-  const char* found_name    = "<out-of-bounds>";
-  if (found < T_LAST)
-    found_name = ctxt_names[found];
-  gedcom_warning(_("Internal error: Unexpected context at %s, line %d: %s"),
-		 file, line, found_name);
-}
-
-void gom_no_context(const char* file, int line)
-{
-  gedcom_warning(_("Internal error: No context at %s, line %d"),
-		 file, line);
-}
-
-void gom_move_error(const char* type)
-{
-  gedcom_warning(_("Could not move struct of type %s"), type);
-}
-
-void gom_find_error(const char* type)
-{
-  gedcom_warning(_("Could not find struct of type %s in chain"), type);
-}
-
 void gom_default_callback (Gedcom_elt elt UNUSED, Gedcom_ctxt parent UNUSED,
 			   int level, char* tag, char* raw_value,
 			   int parsed_tag UNUSED)
 {
   gedcom_warning(_("Data loss in import: \"%d %s %s\""),
                  level, tag, raw_value);
-}
-
-void def_rec_end(Gedcom_rec rec UNUSED, Gedcom_ctxt self,
-		 Gedcom_val parsed_value UNUSED)
-{
-  Gom_ctxt ctxt = (Gom_ctxt)self;
-  destroy_gom_ctxt(ctxt);
-}
-
-/* TODO: do this in a way so that elements out of context can be handled */
-void def_elt_end(Gedcom_elt elt UNUSED, Gedcom_ctxt parent UNUSED,
-		 Gedcom_ctxt self, Gedcom_val parsed_value UNUSED)
-{
-  Gom_ctxt ctxt = (Gom_ctxt)self;
-  destroy_gom_ctxt(ctxt);
 }
 
 void set_xref_type(struct xref_value* xr, const char *str)
